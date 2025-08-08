af08504072a9019b5c68b20ef6c8368713a2e7ac
Natalia <icxodnik@gmail.com>
2025-08-08 16:34:45 +0200

    add implementation

--- src/RateLimitProxy.ts
+++ src/RateLimitProxy.ts
@@ -7,8 +7,20 @@ export function createRateLimitProxy(
   let lastCallTime = 0;
 
   return new Proxy(service, {
-    get(target, prop) {
-      // TODO: Implement the proxy
+    get(target, prop, receiver) {
+      if (prop === 'send') {
+        return function (this: IMessageService, ...args: [string]) {
+          const now = Date.now();
+          if (now - lastCallTime >= intervalMs) {
+            lastCallTime = now;
+            return target.send.call(this, ...args);
+          } else {
+            console.log('[RateLimit] skipped');
+          }
+        };
+      }
+
+      return Reflect.get(target, prop, receiver);
     },
-  });
+  }) as IMessageService;
 }
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,21 +1,49 @@
+function getFormatedDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  let month = (date.getMonth() + 1).toString();
+  let day = date.getDate().toString();
+  let hour = date.getHours().toString();
+  let minute = date.getMinutes().toString();
+  let second = date.getSeconds().toString();
+  if (month.toString().length == 1) {
+    month = '0' + month;
+  }
+  if (day.toString().length == 1) {
+    day = '0' + day;
+  }
+  if (hour.toString().length == 1) {
+    hour = '0' + hour;
+  }
+  if (minute.toString().length == 1) {
+    minute = '0' + minute;
+  }
+  if (second.toString().length == 1) {
+    second = '0' + second;
+  }
+  return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+}
+
 // Декоратор для додавання timestamp
 export function withTimestamp<This, Args extends [string, ...any[]], Return>(
   originalMethod: (this: This, ...args: Args) => Return,
-  context: ClassMethodDecoratorContext<
-    This,
-    (this: This, ...args: Args) => Return
-  >
+  context: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Return>
 ): (this: This, ...args: Args) => Return {
-  // TODO: Implement the decorator
+  return function (this: This, ...args: Args): Return {
+    const message = `[${getFormatedDate()}] ${args[0]}`;
+    const newArgs = [message, ...args.slice(1)] as Args;
+    return originalMethod.call(this, ...newArgs);
+  };
 }
 
 // Декоратор для перетворення в верхній регістр
 export function uppercase<This, Args extends [string, ...any[]], Return>(
   originalMethod: (this: This, ...args: Args) => Return,
-  context: ClassMethodDecoratorContext<
-    This,
-    (this: This, ...args: Args) => Return
-  >
+  context: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Return>
 ): (this: This, ...args: Args) => Return {
-  // TODO: Implement the decorator
+  return function (this: This, ...args: Args): Return {
+    const message = args[0].toUpperCase();
+    const newArgs = [message, ...args.slice(1)] as Args;
+    return originalMethod.call(this, ...newArgs);
+  };
 }
